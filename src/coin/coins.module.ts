13122f30f63365bbeabd9398907549b930fc4eaa
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-06-30 20:34:13 +0800

    Update coins.module.ts

--- src/coin/coins.module.ts
+++ src/coin/coins.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoinController } from './coins.controller';
 import { CoinService } from './coins.service';
-import { User, UserSchema } from './schemas/coin.schema';
+import { Coin, CoinSchema } from './schemas/coin.schema';
+
 
 @Module({
   imports: [
