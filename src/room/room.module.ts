63492ce0334150c108355bd40a218c91469e34b5
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-07-01 19:25:41 +0800

    Update room.module.ts

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CoinController } from './room.controller';
+import { CoinController } from 'namavi/src/room/room.controller';
 import { RoomService } from './room.service';
 import { Room } from './schemas/room.schema';
 
