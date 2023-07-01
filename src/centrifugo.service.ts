840fb7c1cca53659f1db890b1aa40f7230541dd0
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-07-01 22:19:42 +0800

    Update centrifugo.service.ts

--- src/centrifugo.service.ts
+++ src/centrifugo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Centrifuge } from 'centrifuge';
+import { Centrifuge, TransportEndpoint } from 'centrifuge';
 
 @Injectable()
 export class CentrifugoService {
@@ -10,10 +10,14 @@ export class CentrifugoService {
       // debug: true,
       // insecure: true,
     };
-    this.centrifuge = new Centrifuge('http://localhost:8000/connection/websocket');
-    const centrifuge = new Centrifuge('http://localhost:8000/connection/websocket');
+
+    const centrifugeUrl = 'ws://localhost:8000/connection/websocket';
+    this.centrifuge = new Centrifuge(centrifugeUrl, centrifugeOptions);
     
     this.centrifuge.connect();
+    
+  
+    
   }
 
   async connect(): Promise<void> {
@@ -23,17 +27,6 @@ export class CentrifugoService {
   async disconnect(): Promise<void> {
     await this.centrifuge.disconnect();
   }
-
-  // async subscribe(channel: string, callback: (message: any) => void): Promise<void> {
-  //   const subscription = await this.centrifuge.subscribe(channel);
-  //   subscription.on('publish', (message) => {
-  //     callback(message.data);
-  //   });
-  // }
-
-  // async unsubscribe(channel: string): Promise<void> {
-  //   await this.centrifuge.unsubscribe(channel);
-  // }
 
   async publish(channel: string, data: any): Promise<void> {
     await this.centrifuge.publish(channel, data);
