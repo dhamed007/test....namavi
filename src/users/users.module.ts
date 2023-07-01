7492e1ea7a018608c2d45f9f1b8443d6f075c354
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-07-01 22:20:34 +0800

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { UserController } from './users.controller';
 import { UserService } from './users.service';
 import { User, UserSchema } from './schemas/users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CentrifugoService } from 'src/centrifugo.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, CentrifugoService],
 })
 export class UserModule {}
