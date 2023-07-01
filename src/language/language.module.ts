d6f59feb45f7ce0c3f851118778095caabfa1d11
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-07-01 19:08:09 +0800

    Update language.module.ts

--- src/language/language.module.ts
+++ src/language/language.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LanguageController } from './language.controller';
 import { LanguageService } from './language.service';
-import { User, UserSchema } from './schemas/language.schema';
+// import { User, UserSchema } from './schemas/language.schema';
 import { User, UserSchema } from 'src/users/schemas/users.schema';
 
 @Module({
