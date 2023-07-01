8c85ed61a6222701805a899652774e0659b4f931
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2023-07-01 18:39:46 +0800

    Update coin.schema.ts

--- src/coin/schemas/coin.schema.ts
+++ src/coin/schemas/coin.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { User, UserSchema } from './schemas/coin.schema';
+import { User, UserSchema } from './schemas/language.schema';
 
 export type CoinDocument = Coin & Document;
 
